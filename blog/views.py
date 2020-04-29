c6fe412f9f2ae668e5baf48fffdb9ab8db25cb2d
Daniel Walters <daniel.walters2110@gmail.com>
2020-04-29 11:26:15 +0300

    CS django video 5

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'DanielW',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 28th, 2020'
-
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 29th, 2020'
-
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
